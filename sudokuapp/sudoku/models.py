fb6227919436751d6e038082ddd14834d925b394
Austin Jones <austin@campuswise.com>
2017-11-05 00:57:33 -0400

    added Puzzle class to models.py

--- sudokuapp/sudoku/models.py
+++ sudokuapp/sudoku/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Puzzle:
+    data = []
+
+    def get_data(self):
+        return self.data
+
+    def set_data(self):
+        return self.data
